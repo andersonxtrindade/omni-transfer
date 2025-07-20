c69194b9824315b1380e1d4e81b78381ddd63690
Anderson Trindade <axtrindade2012@yahoo.com>
2025-07-20 17:22:18 -0300

    fix: migration configs

--- src/config/ormconfig.migrations.ts
+++ src/config/ormconfig.migrations.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST,
   port: 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
@@ -13,6 +13,12 @@ export const AppDataSource = new DataSource({
   synchronize: false,
   migrationsTableName: 'typeorm_migrations',
   migrationsRun: true,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../**/databases/migrations/*{.ts,.js}'],
 });
