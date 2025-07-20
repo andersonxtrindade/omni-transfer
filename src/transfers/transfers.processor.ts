8903e0e11cd7af1dd6cace5fd483d5ef90094eb0
Anderson Trindade <axtrindade2012@yahoo.com>
2025-07-20 15:28:58 -0300

    feat: add loggers to transfer processor and producer

--- src/transfers/transfers.processor.ts
+++ src/transfers/transfers.processor.ts
@@ -2,20 +2,28 @@ import { Process, Processor } from "@nestjs/bull";
 import { Job } from "bull";
 import { CreateTransferDto } from "./dtos/create-transfer.dto";
 import { TransfersService } from "./transfers.service";
+import { PinoLogger } from 'nestjs-pino';
 
 @Processor('transfer-queue')
 export class TransfersProcessor {
-  constructor(private readonly transfersService: TransfersService) {}
+  constructor(
+    private readonly transfersService: TransfersService,
+    private readonly logger: PinoLogger,
+  ) {
+    this.logger.setContext(TransfersProcessor.name);
+  }
 
   @Process('transfer-job')
   async handleTransferJob(job: Job<CreateTransferDto>) {
+    this.logger.info(`Starting the process of id ${job.id}`);
     const dto = job.data;
 
     try {
       await this.transfersService.processTransfer(dto);
+      this.logger.info(`Job ${job.id} processed successfully`);
     } catch (error) {
-      console.error('Error processing transfer:', error);
-      throw error; 
+      this.logger.error(`Error processing job ${job.id}`, error);
+      throw error;
     }
   }
 }
