bc90df1731f6b7fae1e963854bd280e69d3f3eea
Anderson Trindade <axtrindade2012@yahoo.com>
2025-07-20 15:53:32 -0300

    feat: add loggers to the job

--- src/transfers/transfers.processor.ts
+++ src/transfers/transfers.processor.ts
@@ -16,13 +16,17 @@ export class TransfersProcessor {
   @Process('transfer-job')
   async handleTransferJob(job: Job<CreateTransferDto>) {
     this.logger.info(`Starting the process of id ${job.id}`);
+    job.log(`Starting the process of job ${job.id}`);
+
     const dto = job.data;
 
     try {
       await this.transfersService.processTransfer(dto);
       this.logger.info(`Job ${job.id} processed successfully`);
+      job.log(`Job ${job.id} processed successfully`);
     } catch (error) {
       this.logger.error(`Error processing job ${job.id}`, error);
+      job.log(`Error processing job ${job.id}: ${error.message}`);
       throw error;
     }
   }
